ddf3a996d25525bb9277f3ec5085595844474cf9
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-28 08:59:38 +0900

    ルーティング作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { ChakraProvider, Button } from "@chakra-ui/react";
-import "./styles.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
+
 export default function App() {
   return (
-    <ChakraProvider>
-      <Button colorScheme="teal">ボタン---</Button>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
