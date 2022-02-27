858d7ef16964c2853b528b8460c786333687f3ee
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-27 22:37:59 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { ChakraProvider, Button } from "@chakra-ui/react";
+import "./styles.css";
+export default function App() {
+  return (
+    <ChakraProvider>
+      <Button colorScheme="teal">ボタン</Button>
+    </ChakraProvider>
+  );
+}
