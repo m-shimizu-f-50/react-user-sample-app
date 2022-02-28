b64e90e334c9e2f65df5e616bc49d5ac1e7505a3
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-28 09:40:48 +0900

    Header作成

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import { Login } from "../components/pages/Login";
 import { homeRoutes } from "./HomeRoutes";
 import { Page404 } from "../components/pages/Page404";
-
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 export const Router: VFC = memo(() => {
   return (
     <Switch>
@@ -20,7 +20,7 @@ export const Router: VFC = memo(() => {
                 exact={route.exact}
                 path={`${url}${route.path}`}
               >
-                {route.children}
+                <HeaderLayout>{route.children}</HeaderLayout>
               </Route>
             ))}
           </Switch>
