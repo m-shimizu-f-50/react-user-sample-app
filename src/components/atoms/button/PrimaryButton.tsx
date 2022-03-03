238ca057aa06e331ca29961d9817f86faae4d886
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-03-03 09:52:12 +0900

    ログイン機能の実装

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -3,11 +3,21 @@ import { Button } from "@chakra-ui/react";
 
 type Props = {
   children: ReactNode;
+  onClick: () => void;
+  disabled?: boolean;
+  loading?: boolean;
 };
 export const PrimaryButton: VFC<Props> = memo((props) => {
-  const { children } = props;
+  const { children, onClick, disabled = false, loading = false } = props;
   return (
-    <Button bg="teal.400" color="white" _hover={{ opacity: 0.7 }}>
+    <Button
+      bg="teal.400"
+      color="white"
+      _hover={{ opacity: 0.7 }}
+      onClick={onClick}
+      disabled={disabled || loading}
+      isLoading={loading}
+    >
       {children}
     </Button>
   );
